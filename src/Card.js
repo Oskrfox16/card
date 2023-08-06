b010a33e9faa3d9f0efc3917876448d8d4c9a1be
Oskrfox16 <oskrfox@FOXBrain>
2023-08-05 17:35:54 -0700

    excersise

---
+++ src/Card.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import "./Card.css";
+
+function Card({ name, image }) {
+  
+
+  return <img
+      className="Card"
+      alt={name}
+      src={image}
+       />;
+}
+
+export default Card;
